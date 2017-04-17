84dc1832da8d0988584bc3ec0f7c49563de44c17
Larry Hernandez <larryhernanjr@gmail.com>
2017-04-17 10:47:14 -0400

    add logic for entry sources for reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,36 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+function getEntrySources(sources) {
+  if (process.env.NODE_ENV != 'production') {
+    sources.push('webpack-dev-server/client?http://localhost:8080');
+    sources.push('webpack/hot/only-dev-server');
+  }
+
+  return sources;
+}
+
 module.exports = {
   entry: {
-    helloWorld: './js/helloworld'
+    helloWorld: getEntrySources(['./js/helloworld.js'])
   },
   output: {
+    publicPath: 'http://localhost:8080/',
     filename: 'public/[name].js'
-  }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/, loaders: ['react-hot', 'jsx', 'babel'], exclude: /node_modules/
+      },
+      {
+        test: /\.scss/,
+        loader: ExtractTextPlugin.extract('css!sass')
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('public/style.css', {
+      allChunks: true
+    })
+  ]
 };
